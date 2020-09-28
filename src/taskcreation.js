717aa6f0ea73eff2ca7b2d2e046573d0c661b71f
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-09-28 14:37:55 +0300

    Push the created tasks to a global tasks array

--- src/taskcreation.js
+++ src/taskcreation.js
@@ -1,8 +1,11 @@
-const createtask = (taskname, description, date, priority) => ({
-  taskname,
-  description,
-  date,
-  priority,
-});
+const createtask = (tasksArray, taskname, description, date, priority) => {
+  const todo = {
+    taskname,
+    description,
+    date,
+    priority,
+  };
+  tasksArray.push(todo);
+};
 
 export default createtask;
