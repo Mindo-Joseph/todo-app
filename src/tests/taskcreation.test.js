8ffa535ffa170b86eef030693c1f1d66e99a4ac4
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-12-01 01:28:33 +0300

    Add localstorage mock and implement deletion test

--- src/tests/taskcreation.test.js
+++ src/tests/taskcreation.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 /* eslint-disable no-undef */
 import { getCurrentProjectName, newProject } from '../project';
 import { createtask, deleteTask } from '../taskcreation';
@@ -9,8 +10,11 @@ test('should return an object of user defined todo details', () => {
 test('should remove a task from storage when delete is called on todo name', () => {
   newProject('testingProject');
   const task = createtask('Test', 'some descriptive test', '12-08-2020', 'Low');
-  const arr = JSON.parse(localStorage.getItem('projects'));
-  arr[0][getCurrentProjectName()][0].todos.push(task);
-  deleteTask('Test');
-  expect(arr[0][getCurrentProjectName()][0].todos.length).toBe(0);
+  // eslint-disable-next-line no-underscore-dangle
+  const projects = JSON.parse(localStorage.getItem('projects'));
+  projects[0][getCurrentProjectName()][0].todos.push(task);
+  delete window.location;
+  window.location = { reload: jest.fn() };
+  deleteTask('Task');
+  expect(projects[0][getCurrentProjectName()][0].todos.length).toBe(0);
 });
